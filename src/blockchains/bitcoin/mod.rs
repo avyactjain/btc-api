13ace2401d3eccf6a7181c20f82bdbfc59d1b9df
Avyact Jain <>
2025-02-26 19:28:54 +0530

    working signing and broadcasting flow

--- src/blockchains/bitcoin/mod.rs
+++ src/blockchains/bitcoin/mod.rs
@@ -1,13 +1,27 @@
-use std::str::FromStr;
+use std::{error::Error, str::FromStr};
 
 use axum::Json;
 use bitcoin::{
-    absolute::LockTime, consensus::encode::serialize_hex, transaction::Version, Address, Amount,
-    Network, OutPoint, Script, Transaction, TxIn, TxOut, Txid, Witness,
+    absolute::LockTime,
+    consensus::encode::serialize_hex,
+    hashes::{sha256d, Hash},
+    key::{rand::rngs::OsRng, Secp256k1},
+    secp256k1::{Message, SecretKey},
+    sighash::{Prevouts, SighashCache},
+    transaction::Version,
+    Address, Amount, CompressedPublicKey, EcdsaSighashType, KnownHrp, Network, NetworkKind,
+    OutPoint, PrivateKey, PublicKey, Script, ScriptBuf, Sequence, TxIn, TxOut, Txid, Witness,
 };
-use reqwest::Url;
+
+use bitcoin::blockdata::transaction::Transaction;
+use bitcoincore_rpc::json::SigHashType;
+use reqwest::{Client, Url};
 use response_models::{BlockchaincomResponse, BlockstreamUtxo};
 use tracing::{debug, error, info};
+use utils::{
+    dummy_unspent_transaction_output, receivers_address, senders_keys, CHANGE_AMOUNT,
+    DUMMY_UTXO_AMOUNT, SPEND_AMOUNT,
+};
 
 use crate::{
     btc_api_error::BtcApiError,
@@ -105,11 +119,29 @@ impl Chain for Bitcoin {
         };
 
         match self.create_transaction(transaction_params).await {
-            Ok(transaction) => {
-                let unsigned_raw_txn = serialize_hex(&transaction);
+            Ok((transaction, used_utxos)) => {
+                let raw_txn = serialize_hex(&transaction);
+
+                // Convert the raw transaction to bytes
+                let raw_tx_bytes = hex::decode(raw_txn).expect("Invalid hex transaction");
+
+                // Compute the double SHA-256 hash (32 bytes)
+                let tx_hash = sha256d::Hash::hash(&raw_tx_bytes).to_string();
+
+                // let signed_txn = self
+                //     .sign_transaction(transaction.clone(), used_utxos)
+                //     .await
+                //     .unwrap();
+
+                // self.broadcast_transaction(signed_txn).await.unwrap();
+
+                let x = self.sign_txn_test().await;
+                self.broadcast_transaction(x).await.unwrap();
 
                 result.is_error = false;
-                result.data = Some(CreateTransactionResponseData { unsigned_raw_txn })
+                result.data = Some(CreateTransactionResponseData {
+                    unsigned_raw_txn: tx_hash,
+                })
             }
             Err(err) => {
                 result.error_msg = Some(err.to_string());
@@ -259,7 +291,7 @@ impl Bitcoin {
     async fn create_transaction(
         &self,
         transaction_params: CreateTransactionParams,
-    ) -> Result<Transaction, BtcApiError> {
+    ) -> Result<(Transaction, Vec<BlockstreamUtxo>), BtcApiError> {
         let send_amount = transaction_params.amount;
 
         let receiver_address =
@@ -270,8 +302,8 @@ impl Bitcoin {
             Address::from_str(&transaction_params.from_address)?.require_network(self.network)?;
 
         //1. Get the Txn inputs based on the UTXOs and the change amount
-        let (inputs, change_amount) = self
-            .get_input_txns_and_change_amount(transaction_params)
+        let (inputs, used_utxos, change_amount) = self
+            .get_input_txns_utxos_amounts(transaction_params)
             .await?;
 
         debug!("Inputs: {:#?}", inputs);
@@ -299,7 +331,7 @@ impl Bitcoin {
 
         debug!("Unsigned transaction created: {:#?}", txn);
 
-        Ok(txn)
+        Ok((txn, used_utxos))
     }
 
     async fn find_spendable_utxos(
@@ -319,17 +351,19 @@ impl Bitcoin {
         if blockstream_utxos.is_empty() {
             Err(BtcApiError::NoUtxosFound(address))
         } else {
+            println!("Blockstream UTXOs: {:#?}", blockstream_utxos);
             Ok(blockstream_utxos)
         }
     }
 
-    async fn get_input_txns_and_change_amount(
+    async fn get_input_txns_utxos_amounts(
         &self,
         transaction_params: CreateTransactionParams,
-    ) -> Result<(Vec<TxIn>, u64), BtcApiError> {
+    ) -> Result<(Vec<TxIn>, Vec<BlockstreamUtxo>, u64), BtcApiError> {
         let total_expenditure = transaction_params.amount + transaction_params.fee;
         let mut total_utxo_value = 0;
         let mut inputs = vec![];
+        let mut used_utxos = vec![];
 
         //1. Get the utxos for the from address
         let mut utxos = self
@@ -354,10 +388,172 @@ impl Bitcoin {
 
                 debug!("Added UTXO: {:#?}", utxo);
                 total_utxo_value += utxo.value;
+                used_utxos.push(utxo);
             }
         }
 
-        Ok((inputs.to_vec(), total_utxo_value - total_expenditure))
+        Ok((
+            inputs.to_vec(),
+            used_utxos,
+            total_utxo_value - total_expenditure,
+        ))
+    }
+
+    async fn sign_txn_test(&self) -> String {
+        let secp = Secp256k1::new();
+
+        // Get a secret key we control and the pubkeyhash of the associated pubkey.
+        // In a real application these would come from a stored secret.
+        let (sk, wpkh) = senders_keys(&secp);
+
+        // Get an address to send to.
+        let address = receivers_address();
+
+        // Get an unspent output that is locked to the key above that we control.
+        // In a real application these would come from the chain.
+        let (dummy_out_point, dummy_utxo) = dummy_unspent_transaction_output(wpkh);
+
+        // The input for the transaction we are constructing.
+        let input = TxIn {
+            previous_output: dummy_out_point, // The dummy output we are spending.
+            script_sig: ScriptBuf::default(), // For a p2wpkh script_sig is empty.
+            sequence: Sequence::ZERO,
+            witness: Witness::default(), // Filled in after signing.
+        };
+
+        // The spend output is locked to a key controlled by the receiver.
+        let spend = TxOut {
+            value: SPEND_AMOUNT,
+            script_pubkey: address.script_pubkey(),
+        };
+
+        // The change output is locked to a key controlled by us.
+        let change = TxOut {
+            value: CHANGE_AMOUNT,
+            script_pubkey: ScriptBuf::new_p2wpkh(&wpkh), // Change comes back to us.
+        };
+
+        // The transaction we want to sign and broadcast.
+        let mut unsigned_tx = Transaction {
+            version: Version::TWO,       // Post BIP-68.
+            lock_time: LockTime::ZERO,   // Ignore the locktime.
+            input: vec![input],          // Input goes into index 0.
+            output: vec![spend, change], // Outputs, order does not matter.
+        };
+        let input_index = 0;
+
+        // Get the sighash to sign.
+        let sighash_type = EcdsaSighashType::All;
+        let mut sighasher = SighashCache::new(&mut unsigned_tx);
+        let sighash = sighasher
+            .p2wpkh_signature_hash(
+                input_index,
+                &dummy_utxo.script_pubkey,
+                DUMMY_UTXO_AMOUNT,
+                sighash_type,
+            )
+            .expect("failed to create sighash");
+
+        // Sign the sighash using the secp256k1 library (exported by rust-bitcoin).
+        let msg = Message::from(sighash);
+        let signature = secp.sign_ecdsa(&msg, &sk);
+
+        // Update the witness stack.
+        let signature = bitcoin::ecdsa::Signature {
+            signature,
+            sighash_type,
+        };
+        let pk = sk.public_key(&secp);
+        *sighasher.witness_mut(input_index).unwrap() = Witness::p2wpkh(&signature, &pk);
+
+        // Get the signed transaction.
+        let tx = sighasher.into_transaction();
+
+        // BOOM! Transaction signed and ready to broadcast.
+        println!("{:#?}", tx);
+
+        let x = serialize_hex(&tx);
+        println!("Signed Transaction: {}", x);
+        x
+    }
+
+    // async fn sign_transaction(
+    //     &self,
+    //     mut unsigned_txn: Transaction,
+    //     utxos: Vec<BlockstreamUtxo>,
+    // ) -> Result<String, BtcApiError> {
+    //     let secp = Secp256k1::new();
+
+    //     let wif = "cSjgVro2xkCVat8fjye1jNfozoaC8XASd3UuvLXF49ugaZx1MHsg"; // Replace with your private key
+    //     let private_key = PrivateKey::from_wif(wif).expect("Invalid WIF key");
+    //     let public_key = private_key.public_key(&secp);
+
+    //     let mut signatures = vec![];
+
+    //     {
+    //         let mut sighash_cache = SighashCache::new(&unsigned_txn);
+
+    //         for (i, utxo) in utxos.iter().enumerate() {
+    //             let pubkey = CompressedPublicKey::from_private_key(&secp, &private_key).unwrap();
+    //             println!("Pubkey: {:#?}", pubkey);
+    //             let address = Address::p2wpkh(&pubkey, Network::Testnet);
+    //             let script_pubkey = address.script_pubkey();
+    //             println!("Script Pubkey: {:#?}", script_pubkey);
+    //             println!("Address: {:#?}", address);
+
+    //             let sighash_msg = sighash_cache
+    //                 .p2wpkh_signature_hash(
+    //                     0,
+    //                     script_pubkey.as_script(),
+    //                     Amount::from_sat(utxo.value),
+    //                     bitcoin::EcdsaSighashType::All,
+    //                 )
+    //                 .unwrap();
+
+    //             let msg =
+    //                 Message::from_digest_slice(&sighash_msg[..]).expect("Failed to create message");
+
+    //             let sig = secp.sign_ecdsa(&msg, &private_key.inner);
+
+    //             let mut sig_with_sighash = sig.serialize_der().to_vec();
+    //             sig_with_sighash.push(bitcoin::EcdsaSighashType::All as u8);
+    //             signatures.push((i, sig_with_sighash, pubkey.to_bytes()));
+    //         }
+    //     }
+
+    //     println!("Signatures: {:#?}", signatures);
+    //     // 🔹 Apply Signatures to Transaction
+    //     for (i, sig, pubkey) in signatures {
+    //         unsigned_txn.input[i].witness.push(sig);
+    //         unsigned_txn.input[i].witness.push(pubkey);
+    //     }
+
+    //     println!("Signed Transaction: {:#?}", unsigned_txn);
+    //     let x = unsigned_txn.clone();
+    //     println!("Signed Transaction: {:#?}", serialize_hex(&x));
+    //     // 🔹 Serialize & Print Signed Transaction
+    //     let raw_tx_hex = serialize_hex(&unsigned_txn);
+    //     println!("Signed Transaction: {}", raw_tx_hex);
+
+    //     Ok(raw_tx_hex)
+    // }
+
+    async fn broadcast_transaction(&self, signed_txn_hash: String) -> Result<(), Box<dyn Error>> {
+        println!("Broadcasting transaction");
+        // 2️⃣ Blockstream API endpoint for Testnet
+        let url = "https://blockstream.info/testnet/api/tx";
+
+        // 3️⃣ Send the transaction using an HTTP POST request
+        let client = Client::new();
+        let response = client.post(url).body(signed_txn_hash).send().await?;
+
+        // 4️⃣ Print the Transaction ID (TxID)
+        println!(
+            "✅ Transaction broadcasted! TxID: {}",
+            response.text().await?
+        );
+
+        Ok(())
     }
 }
 
--- src/blockchains/bitcoin/utils.rs
+++ src/blockchains/bitcoin/utils.rs
@@ -1,1 +1,91 @@
+use std::str::FromStr;
 
+use bitcoin::{
+    hashes::Hash,
+    key::{rand::rngs::OsRng, Secp256k1},
+    secp256k1::{SecretKey, Signing},
+    Address, Amount, CompressedPublicKey, KnownHrp, Network, NetworkKind, OutPoint, PrivateKey,
+    ScriptBuf, TxOut, Txid, WPubkeyHash,
+};
+use secp256k1::rand;
+
+pub const DUMMY_UTXO_AMOUNT: Amount = Amount::from_sat(6600);
+pub const SPEND_AMOUNT: Amount = Amount::from_sat(1000);
+pub const CHANGE_AMOUNT: Amount = Amount::from_sat(5400); // 100 sat fee.
+
+pub fn create_testnet_wallet() -> (PrivateKey, Address) {
+    //1.Create a random private key
+    let secp = Secp256k1::new();
+    let mut rng = OsRng;
+    let secret_key = SecretKey::new(&mut rng);
+
+    //2. Convert it into a Bitcoin PrivateKey (Testnet)
+    let private_key = PrivateKey {
+        compressed: true,
+        network: NetworkKind::Test,
+        inner: secret_key,
+    };
+
+    //3️. Generate a compressed Public Key
+    let public_key = CompressedPublicKey::from_private_key(&secp, &private_key).unwrap();
+
+    //4. Generate a Testnet Bech32 Address (P2WPKH)
+    let address = Address::p2wpkh(&public_key, KnownHrp::Testnets);
+
+    // 🔹 Print the wallet details
+    println!("🔑 Private Key (WIF): {}", private_key.to_wif());
+    println!("📫 Testnet Address: {}", address);
+
+    (private_key, address)
+}
+
+/// An example of keys controlled by the transaction sender.
+///
+/// In a real application these would be actual secrets.
+pub fn senders_keys<C: Signing>(secp: &Secp256k1<C>) -> (SecretKey, WPubkeyHash) {
+    let private_key = PrivateKey::from_wif("cSjgVro2xkCVat8fjye1jNfozoaC8XASd3UuvLXF49ugaZx1MHsg")
+        .expect("invalid wif");
+    let sk = private_key.inner;
+    let pk = bitcoin::PublicKey::new(sk.public_key(secp));
+    let wpkh = pk.wpubkey_hash().expect("key is compressed");
+
+    (sk, wpkh)
+}
+
+/// A dummy address for the receiver.
+///
+/// We lock the spend output to the key associated with this address.
+///
+/// (FWIW this is a random mainnet address from block 80219.)
+pub fn receivers_address() -> Address {
+    "tb1qpuv7hp0khx0u65k2868nksx6gxynfgn88rd2gs"
+        .parse::<Address<_>>()
+        .expect("a valid address")
+        .require_network(Network::Testnet)
+        .expect("valid address for testnet")
+}
+
+/// Constructs a new p2wpkh output locked to the key associated with `wpkh`.
+///
+/// An utxo is described by the `OutPoint` (txid and index within the transaction that it was
+/// created). Using the out point one can get the transaction by `txid` and using the `vout` get the
+/// transaction value and script pubkey (`TxOut`) of the utxo.
+///
+/// This output is locked to keys that we control, in a real application this would be a valid
+/// output taken from a transaction that appears in the chain.
+pub fn dummy_unspent_transaction_output(wpkh: WPubkeyHash) -> (OutPoint, TxOut) {
+    let script_pubkey = ScriptBuf::new_p2wpkh(&wpkh);
+
+    let out_point = OutPoint {
+        txid: Txid::from_str("f924b4293fd009ed51f4ec69c60ae16b6d6d79616d1b76cc01a443c969d69f00")
+            .unwrap(), // Arbitrary invalid dummy value.
+        vout: 1,
+    };
+
+    let utxo = TxOut {
+        value: DUMMY_UTXO_AMOUNT,
+        script_pubkey,
+    };
+
+    (out_point, utxo)
+}
